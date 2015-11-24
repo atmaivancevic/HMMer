d3a9dc16f3739230e8f7b30c73cf22c705e583d9
Atma <atma.ivancevic@adelaide.edu.au>
2015-11-24 15:24:40 +1030

    added a step to domains.sh that puts the output in latex-compilable form (e.g. \ before _, etc).

--- domains.sh
+++ domains.sh
@@ -9,3 +9,9 @@ awk '{print $1}' $1.domains | sort -u > $1.domains_filtered
 
 # convert column of results into a comma-and-space separated list
 cat $1.domains_filtered | xargs | sed -e 's/ /, /g' > $1.domains_list
+
+# replace #, $, %, & and _ with \#, \$, \%, \& and \_, respectively
+sed -E 's/([#$%&_\])/\\&/g' $1.domains_list > $1.latex_domains
+
+# remove unwanted files
+rm $1.domains_list $1.domains_filtered
