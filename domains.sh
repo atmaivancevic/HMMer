f531dceb7c735b3fb620ca95ece3092e6f6a2984
Atma <atma.ivancevic@adelaide.edu.au>
2015-11-24 12:27:39 +1030

    added to a step to domains.sh which converts the output from column to comma-separated list format

--- domains.sh
+++ domains.sh
@@ -5,5 +5,7 @@
 # After removing all the non-ORF1s, and deleting them from $1.domains, 
 # run this to extract the all the unique domains
 # found in the remaining (correct) ORF1s. 
-
 awk '{print $1}' $1.domains | sort -u > $1.domains_filtered
+
+# convert column of results into a comma-and-space separated list
+cat $1.domains_filtered | xargs | sed -e 's/ /, /g' > $1.domains_list
